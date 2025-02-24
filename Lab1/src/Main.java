88b31a864955071ee4f0964222c464b4e0598ed5
teoiac <teo_iacob08@yahoo.com>
2025-02-24 14:47:39 +0200

    Tema 1 - to do: reprezentare cu unicode

--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -9,22 +9,21 @@ public class Main {
         String[] languages = new String[]{"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
         //Exercitiul 3 : Genereaza random integer n
         int n;
-        n = (int) (Math.random() *1_000_000);
+        n = (int) (Math.random() * 1_000_000);
         System.out.println("n = " + n);
-        n=n*3;
+        n = n * 3;
         n += 0b10101;
-        n+=0xFF;
-        n*=6;
+        n += 0xFF;
+        n *= 6;
         System.out.println("n = " + n);
-        while(n>=10)
-        {
-            n=sumOfDigits(n);
+        while (n >= 10) {
+            n = sumOfDigits(n);
         }
         System.out.println("Willy-nilly, this semester I will learn " + languages[n]);
     }
 
     private static int sumOfDigits(int n) {
-        int sum = 0;
+        int sum =  0;
         while (n > 0) {
             sum += n % 10;
             n /= 10;
---
+++ Tema-1/src/Main.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i, j, edgesCount = 0;
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[][] graph = new int[n][n];
+        for (i = 0; i < n; i++)
+            for (j = 0; j < n; j++)
+                graph[i][j] = -1;
+
+
+        for (i = 0; i < k; i++)
+            for (j = 0; j < k; j++) {
+                graph[i][j] = 1;
+                graph[j][i] = 1;
+            }
+
+        if (n - k >= k) {
+            for (i = k; i < n; i++) {
+                for (j = k; j < n; j++) {
+                    graph[i][j] = 0;
+                    graph[j][i] = 0;
+                }
+            }
+        } else {
+            System.out.println("Nu putem avea si multime stabila");
+        }
+        for (i = 0; i < n; i++) {
+            graph[i][i] = 0;
+        }
+        for (i = 0; i < n; i++)
+            for (j = 0; j < n; j++) {
+                if (graph[i][j] == -1) {
+                    graph[i][j] = (int) (Math.random() * (2 - 0) + 0);
+                    graph[j][i] = graph[i][j];
+                }
+            }
+        int minDegree = Integer.MAX_VALUE;
+        int maxDegree = Integer.MIN_VALUE;
+        for (i = 0; i < n; i++) {
+            int degree = 0;
+            for (j = 0; j < n; j++) {
+                System.out.print(graph[i][j] + " ");
+                if (graph[i][j] == 1) {
+                    edgesCount++;
+                    degree++;
+                }
+
+            }
+            if (degree < minDegree)
+                minDegree = degree;
+            if (degree > maxDegree)
+                maxDegree = degree;
+            System.out.println();
+        }
+        edgesCount = edgesCount / 2;
+        System.out.println("Numarul de muchii = " + edgesCount);
+
+        System.out.println("Gradul maxim = " + maxDegree);
+
+        System.out.println("Gradul minim = " + minDegree);
+
+    }
+}
